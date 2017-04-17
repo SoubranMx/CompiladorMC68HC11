c6e1e5844b9d1b7d4f7030a126f51979f639a044
SoubranMx <uriel_bee15@hotmail.com>
2017-04-17 01:22:55 -0500

    Avance de conteo de elementos
    Creo que ya funciona el método unElemento, las palabras se cuentan
    mientras se cuentan las lineas al principio.

--- CompiladorEstructuras/src/compiladorestructuras/FileRead.java
+++ CompiladorEstructuras/src/compiladorestructuras/FileRead.java
@@ -12,6 +12,9 @@ public class FileRead {
     private List<String> code;                  //0 tabuladores o variables / 1- mnemonico o directiva / 3- direcciones o instrucciones / 4- Comentarios (de haberlos en la misma linea)
     private int n_lineas;
     
+    public String palabra;
+    public List<Integer> n_palabras;   // cuenta las palabras por linea
+    
     //CONSTRUCTOR
     public FileRead(){
         int i;
@@ -22,25 +25,45 @@ public class FileRead {
         for(i=0;i<=3;i++)
             code.add("");   //Aqui logra crear los espacios que corresponderian a [x..][0..1..2..3]
         s = new Scanner (System.in);
+        palabra = "";
+        n_palabras = new ArrayList<Integer>();
     }
     
     //METODOS
     private void cuentaLineas(){
         // Pide el nombre del archivo, lo guarda, abre un FileReader llamado archi y cuenta lineas hasta que encuentra 13, que es el ascii del enter
         //el tab es un 9 ascii, 13 10 enter, 32 space
-        int cuenta;
+        int Byte, n_lin=0, pal = 0;
         try{
+            //el tab es un 9 ascii, 13 10 enter, 32 space, 42 asterisco
             System.out.println("Ingrese el nombre del archivo: ");
             archivo = s.nextLine();
             FileReader archi = new FileReader(archivo);
-            cuenta = archi.read();
-            while(cuenta!=-1){
-                if(cuenta == 13)
-                    n_lineas++;
-                cuenta = archi.read();
+            Byte = archi.read();
+            while(Byte!=-1){
+                if(Byte != 42 && Byte != 13){
+                    if(Byte == 32 || Byte == 9){
+                        while(Byte == 32 || Byte == 9)
+                            Byte = archi.read();
+                        while(Byte!=13){
+                            pal++;
+                            while(Byte != 32 && Byte != 9 && Byte != 13 )
+                                Byte = archi.read();
+                        }
+                    }
+                }
+                else if (Byte == 42){
+                    while(Byte != 13)
+                        Byte = archi.read();
+                }
+                //if(Byte == 13)
+                n_lin++;
+                Byte = archi.read();    //Siguiente linea
+                n_palabras.add(pal);    //cada localidad será una linea del codigo de forma n-1, get(x) me dará el numero de palabras en cada linea
             }
-            n_lineas++;
-            System.out.println("Lineas: "+n_lineas);
+            n_lin++;
+            //System.out.println("Lineas: "+n_lineas);
+            //setNLin(n_lin);
             archi.close();
         }catch(IOException e){
             System.out.println(e);
@@ -51,20 +74,74 @@ public class FileRead {
         int i;
         this.guardaCodigo.add(mne);
     };
+    public int unElemento(int eByte,ArrayList<String>nemu,FileReader f){
+        try{
+            if(eByte == 32 || eByte == 9){    //Hay un espacio o tabulador al principio
+                nemu.add(0,"0");
+            }
+            else{
+                while(eByte != 9 && eByte != 32 && eByte != 13){     //concatena la palabra que encuentre hasta que haya un espacio
+                    palabra = palabra.concat(""+(char)eByte);
+                    eByte = f.read();
+                }
+                //en este enteroByte debe ser un enter
+                nemu.add(0,palabra);    //matriz[0][i] = palabra;
+                eByte = addRepetido(1,palabra, eByte, f, nemu);    //Solo es un elemento, guarda lo demas en 0
+                /*if(eByte == 13){       //enter es 13 10, enteroByte en este punto es 13
+                    eByte = f.read();      //enteroByte en este punto es 10
+                    eByte = f.read();      //enteroByte en este punto debe ser nueva linea
+                }*/
+                /*else{
+                    //error
+                }*/
+            }
+            palabra = "";
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+        return eByte;
+    };
+    /*public int dosElemento(){
+        return eByte;
+    };
+    public int tresElemento(){
+        return eByte;
+    };
+    public int cuatroElemento(){
+        return eByte;
+    };*/
     
     public void fileR(){
-        String palabra="";
         ArrayList<String> nemo = new ArrayList<String>();
-        //char car;
-        int enteroByte = 0,i=0,j;
+        int enteroByte = 0,i=0,swch;
         
         try{
             FileReader f = new FileReader(archivo);
             enteroByte = f.read();
-            //el tab es un 9 ascii, 13 10 enter, 32 space, 42 asterisco
             while(enteroByte!=-1){
+                swch = n_palabras.get(i);
+                switch(i){
+                    case 1:
+                        enteroByte = unElemento(enteroByte,nemo,f);
+                        break;
+                    /*case 2:
+                        enteroByte = dosElemento(enteroByte,nemo,f);
+                        break;
+                    case 3:
+                        enteroByte = tresElemento(enteroByte,nemo,f);
+                        break;
+                    case 4:
+                        enteroByte = cuatroElemento(enteroByte,nemo,f);
+                        break;*/
+                }
+                i++;
+            }
+            
+            
+            
             //  **** Para matriz [0][x] *****
-                if(enteroByte == 32 || enteroByte == 9){    //Hay un espacio o tabulador al principio
+                /*if(enteroByte == 32 || enteroByte == 9){    //Hay un espacio o tabulador al principio
                     nemo.add(0,"0");      //equivale a matriz[i][0] = "0";
                 }
                 else if (enteroByte !=42) {     //Se asegura que no es un tabulador, ni un asterisco al principio
@@ -86,6 +163,8 @@ public class FileRead {
                 palabra = "";
             //En este punto, ya se tiene un elemento en la posicion 0 de nemo
             
+            
+            
 //          ******* Para matriz[1][x] *******
                 while(enteroByte != 9 || enteroByte != 32){
                     palabra = palabra.concat(""+(char)enteroByte);
@@ -102,6 +181,10 @@ public class FileRead {
                 nemo.add(2,palabra);    //matriz[1][i] = palabra;
                 enteroByte = addRepetido(3,palabra, enteroByte, f, nemo);    //3 elementos
                 palabra = "";
+            
+            
+            
+            
             // ******* Para matriz[3][x] ****
                 if(enteroByte == 42){   //Es un comentario al final de la linea, se toma en cuenta
                     while(enteroByte!=13){
@@ -113,9 +196,9 @@ public class FileRead {
                 else{
                     //error
                 }
-                //Ya se leyó toda la linea
+                //Ya se leyó toda la linea*/
                 
-            }
+            //}
             System.out.println();
             /*for(i=0;i<3;i++)
                System.out.print(matriz[0][i]+i+"\t");*/
@@ -125,11 +208,19 @@ public class FileRead {
     }
     
     public int addRepetido(int x, String palabra,int enteroByte, FileReader f, ArrayList<String>nemr){
-        while(enteroByte == 9 || enteroByte == 32)  //me aseguro que enteroByte pase a la siguiente palabra o llegue a un enter
-            enteroByte = f.read();
-        if(enteroByte == 13){
-            for(i=x;i<=3;i++)
-                nemr.add(x,"0");
+        int i;
+        try{
+            while(enteroByte == 9 || enteroByte == 32)  //me aseguro que enteroByte pase a la siguiente palabra o llegue a un enter
+                enteroByte = f.read();
+            if(enteroByte == 13){
+                for(i=x;i<=3;i++)
+                    nemr.add(x,"0");
+                enteroByte = f.read();      //10
+                enteroByte = f.read();      //Sig linea
+            }
+        }
+        catch(IOException e){
+            System.out.println(e);
         }
         return enteroByte;
     };
